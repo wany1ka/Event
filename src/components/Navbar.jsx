16e1829d856fbe71da72ad9bc8e2909e70146136
wany1ka <markwanyika@gmail.com>
2024-04-22 11:32:03 +0300

    add components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+    return (
+        <nav className="bg-gray-800 py-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <li>
+                    <Link to="/" className="text-white hover:text-gray-300">Home</Link>
+                </li>
+                <ul className="flex justify-center space-x-4">
+
+                    <li>
+                        <Link to="/about" className="text-white hover:text-gray-300">About</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
